10abb900d4c2c98b8883e7e4a90b52f4f1bd5b77
duke-phan <duke.phan@playstudios.asia>
2026-09-21 19:27:12 +0700

    refactor

--- src/game/main.ts
+++ src/game/main.ts
@@ -7,25 +7,25 @@ import MainMenu from './scenes/main/MainMenu';
 import GamesMenu from './scenes/games/GamesMenu';
 import HSKDataService from "../services/HSKDataService";
 
-import VocabularyMenu from './scenes/learning/vocabulary/VocabularyMenu';
+import VocabularyMenu from './scenes/vocabulary/VocabularyMenu';
 
-import HSKLevelMenu from './scenes/learning/vocabulary/hsk/HSKLevelMenu';
-import HSKHome from './scenes/learning/vocabulary/hsk/HSKHome';
+import HSKLevelMenu from './scenes/vocabulary/learn/hsk/HSKLevelMenu';
+import HSKHome from './scenes/vocabulary/learn/hsk/HSKHome';
 
-import VocabularyScene from './scenes/learning/vocabulary/hsk/words/VocabularyScene';
-import WordDetailScene from './scenes/learning/vocabulary/hsk/words/WordDetailScene';
+import VocabularyScene from './scenes/vocabulary/learn/hsk/words/VocabularyScene';
+import WordDetailScene from './scenes/vocabulary/learn/hsk/words/WordDetailScene';
 
-import GrammarScene from './scenes/learning/vocabulary/hsk/grammar/GrammarScene';
-import GrammarDetailScene from './scenes/learning/vocabulary/hsk/grammar/GrammarDetailScene';
+import GrammarScene from './scenes/vocabulary/learn/hsk/grammar/GrammarScene';
+import GrammarDetailScene from './scenes/vocabulary/learn/hsk/grammar/GrammarDetailScene';
 
-import HanziScene from './scenes/learning/vocabulary/hsk/hanzi/HanziScene';
-import HanziDetailScene from './scenes/learning/vocabulary/hsk/hanzi/HanziDetailScene';
+import HanziScene from './scenes/vocabulary/learn/hsk/hanzi/HanziScene';
+import HanziDetailScene from './scenes/vocabulary/learn/hsk/hanzi/HanziDetailScene';
 
-import TopicScene from './scenes/learning/vocabulary/hsk/topics/TopicScene';
-import TopicDetailScene from './scenes/learning/vocabulary/hsk/topics/TopicDetailScene';
+import TopicScene from './scenes/vocabulary/learn/hsk/topics/TopicScene';
+import TopicDetailScene from './scenes/vocabulary/learn/hsk/topics/TopicDetailScene';
 
-import TaskScene from './scenes/learning/vocabulary/hsk/tasks/TaskScene';
-import TaskDetailScene from './scenes/learning/vocabulary/hsk/tasks/TaskDetailScene';
+import TaskScene from './scenes/vocabulary/learn/hsk/tasks/TaskScene';
+import TaskDetailScene from './scenes/vocabulary/learn/hsk/tasks/TaskDetailScene';
 
 //  Find out more information about the Game Config at:
 //  https://docs.phaser.io/api-documentation/typedef/types-core#gameconfig
